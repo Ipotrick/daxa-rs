67d8265ac8a9978064c7941e6fdac6eec3f43ace
Anish Bhobe <anishbhobe@hotmail.com>
2023-11-03 06:25:28 +0100

    Added swapchain functions functions.
    TODO Refcounting swapchain

--- src/swapchain.rs
+++ src/swapchain.rs
@@ -1,4 +1,4 @@
-use crate::types;
+use crate::types::{self, Swapchain};
 
 type NativeWindowHandle = daxa_sys::daxa_NativeWindowHandle;
 
@@ -8,7 +8,7 @@ pub extern "C" fn default_format_selector(format: types::Format) -> i32 {
     unsafe { daxa_sys::daxa_default_format_selector(format as _) }
 }
 
-#[repr(u32)]
+#[repr(i32)]
 pub enum NativeWindowPlatform {
     Unknown = daxa_sys::daxa_NativeWindowPlatform_DAXA_NATIVE_WINDOW_PLATFORM_UNKNOWN,
     Win32 = daxa_sys::daxa_NativeWindowPlatform_DAXA_NATIVE_WINDOW_PLATFORM_WIN32_API,
@@ -16,7 +16,7 @@ pub enum NativeWindowPlatform {
     Wayland = daxa_sys::daxa_NativeWindowPlatform_DAXA_NATIVE_WINDOW_PLATFORM_WAYLAND_API,
 }
 
-#[repr(u32)]
+#[repr(i32)]
 pub enum PresentMode {
     Immediate = daxa_sys::VkPresentModeKHR_VK_PRESENT_MODE_IMMEDIATE_KHR,
     Mailbox = daxa_sys::VkPresentModeKHR_VK_PRESENT_MODE_MAILBOX_KHR,
@@ -52,4 +52,51 @@ pub struct SwapchainInfo {
     image_usage: types::ImageUsageFlags,
     max_allowed_frames_in_flight: usize,
     name: types::SmallString,
+}
+
+macro_rules! swapchain_fn {
+    ($name:ident, $ret:ty) => {
+        paste::item! {
+            pub fn $name (&self) -> $ret {
+                unsafe {
+                    std::mem::transmute(daxa_sys::[< daxa_swp_ $name >] (self.0))
+                }
+            }
+        }
+    };
+}
+
+impl Swapchain {
+    pub fn set_present_mode(&self, present_mode: PresentMode) -> types::Result {
+        unsafe { std::mem::transmute(daxa_sys::daxa_swp_set_present_mode(self.0, present_mode)) }
+    }
+
+    pub fn acquire_next_image(&self) -> std::result::Result<types::ImageId, types::Result> {
+        unsafe {
+            let mut out_image_id: types::ImageId;
+            let res = std::mem::transmute(daxa_sys::daxa_swp_acquire_next_image(
+                self.0,
+                out_image_id.0,
+            ));
+            if res == types::Result::Success {
+                Ok(out_image_id)
+            } else {
+                Err(res)
+            }
+        }
+    }
+
+    swapchain_fn!(get_surface_extent, types::Extent);
+    swapchain_fn!(get_format, types::Format);
+    swapchain_fn!(resize, types::Result);
+    swapchain_fn!(get_acquire_semaphore, types::BinarySemaphore);
+    swapchain_fn!(get_present_semaphore, types::BinarySemaphore);
+    swapchain_fn!(get_gpu_timeline_semaphore, types::TimelineSemaphore);
+    swapchain_fn!(get_acquire_semaphore, types::BinarySemaphore);
+    swapchain_fn!(get_cpu_timeline_value, usize);
+    swapchain_fn!(info, SwapchainInfo);
+    swapchain_fn!(get_vk_swapchain, daxa_sys::VkSwapchainKHR);
+    swapchain_fn!(get_vk_surface, daxa_sys::VkSurfaceKHR);
+    swapchain_fn!(inc_refcnt, u64);
+    swapchain_fn!(dec_refcnt, u64);
 }
